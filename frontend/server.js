84fa0f1e1344a2568c7e9086b01d37f1beb89c46
Yashar Baradarvar <83546651+DavidYashar@users.noreply.github.com>
2023-09-14 10:01:36 +0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
 const Customer = require('./module/customer');
+const bcrypt = require('bcrypt');
+
 const cors = require('cors');
 server = express();
 // const bodyParser = require("body-parser");
@@ -11,6 +13,7 @@ const DB = 'mongodb+srv://Yashar:33456Yashar@cluster0.wcrhxar.mongodb.net/custom
 mongoose.connect(DB)
 .then((result)=> console.log('connected'+ result)
 )
+
 
 .catch((err)=> console.log(err))
 
@@ -27,16 +30,80 @@ if(server.listen(3100)){
 
 
 
-server.post('/receive', (req, res)=> {
-    const body = new Customer(req.body);
-    body.save()
-    // res.send('ok, received');
-    console.log('saved')
-    res.json(body.names);
 
+
+
+server.post('/', async (req, res)=> {
+    const {email, password}= req.body;
+    
+    try{
+        const user =await Customer.findOne({email: email});
+        
+        if(!user){
+            res.json( 'authentication failed');
+            console.log('not found')
+        }else{
+
+            bcrypt.compare(password, user.password, (err, result)=> {
+                if(err){
+                    console.error(err);
+                    res.status(500).json('internal server error');
+                }else if(result){
+                    console.log(user+ ' found')
+                    res.json('welcome');
+                }else{
+                    console.log('password incorrect');
+                    res.json('password incorrect')
+                }
+            })
+            
+           
+        }
+    }catch(err){
+        console.error(err.message)
+        res.status(500).json('Internal Server Error');
+    }
+    
+
+
+   
 })
 
 
-server.get('/', (req, res)=> {
-    res.send('sending info')
+
+
+
+server.post('/receive', async (req, res)=> {
+
+    // const received = req.body;
+try{
+
+    const hashed =await bcrypt.hash(req.body.password, 10);
+
+    const data = {
+        names: req.body.names,
+        email: req.body.email,
+        socialSecurity: req.body.socialSecurity,
+        password:  hashed
+    }
+
+    
+   
+            const body = new Customer(data);
+            
+             body.save();
+            res.json('registered')
+            console.log('saved')
+    
+    
+}catch(err){
+    console.error(err)
+}
+    
+
+   
+    // res.send('ok, received');
+    // console.log('saved')
+    // res.json(body.names);
+
 })
