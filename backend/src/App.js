aaf3dfa9b0ca37a8055b7fff64e72d893b977e30
Yashar Baradarvar <83546651+DavidYashar@users.noreply.github.com>
2023-09-24 06:23:58 +0300

    Update App.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import Main from './main';
 import Login from './Login';
 import Home from './Home';
-
-
+import Atm from './Atm';
+import Deposit from './Deposit';
+import Withdraw from './Withdraw';
+import Transfer from './Transfer';
 function App() {
   return (
     <div className="App">
      <Router>
       <Routes>
-        <Route exact path='/' Component={Main}/>
-        <Route path='/Login' Component={Login}/>
+        <Route exact path='/' Component={Login}/>
+        <Route path='/SignUp' Component={Main}/>
         <Route path='/Home' Component={Home}/>
+        <Route path='/Atm' Component={Atm}/>
+        <Route path='/Deposit' Component={Deposit}/>
+        <Route path='/Withdraw' Component={Withdraw}/>
+        <Route path='/Transfer' Component={Transfer}/>
       </Routes>
      </Router>
     </div>
