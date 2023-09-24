70515c61ad318fc8c2e8b419e2a0ed081d48df67
Yashar Baradarvar <83546651+DavidYashar@users.noreply.github.com>
2023-09-24 06:24:57 +0300

    Create Atm.js

---
+++ backend/src/Atm.js
@@ -0,0 +1,52 @@
+// import { Link } from "react-router-dom";
+
+const Atm = () => {
+
+  const  handleDeposit = ()=>{
+    setTimeout(()=> {
+        window.location.href='/Deposit'
+    }, 500)
+  } 
+
+  const  handleWithdraw = ()=>{
+    setTimeout(()=> {
+        window.location.href='/Withdraw'
+    }, 500)
+  } 
+
+  const  handleTransfer = ()=>{
+    setTimeout(()=> {
+        window.location.href='/Transfer'
+    }, 500)
+  } 
+
+    return ( 
+        <div className="atm">
+          <h3>Welcome, How we may Help you?</h3>
+
+
+          <div className="deposit"><br />
+            <button onClick={handleDeposit}>Deposit</button>
+          </div>
+
+          <div className="withdraw"><br />
+            <button onClick={handleWithdraw}>Withdraw</button>
+          </div>
+
+          
+
+          <div className="transfer"><br />
+            <button onClick={handleTransfer}>Transfer</button>
+          </div><br />
+
+          <div className="eject">
+            <button>Eject</button>
+          </div>
+
+
+
+        </div>
+     );
+}
+ 
+export default Atm;
